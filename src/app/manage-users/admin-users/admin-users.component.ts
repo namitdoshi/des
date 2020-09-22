5941bb828787146de694427a5b23014e4a4fd419
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-09-22 23:00:52 +0530

    admin users created without navtab 'add user' button and edit icon

--- src/app/manage-users/admin-users/admin-users.component.ts
+++ src/app/manage-users/admin-users/admin-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-admin-users',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-users.component.scss']
 })
 export class AdminUsersComponent implements OnInit {
+  displayedColumns = ['edit', 'name', 'username', 'role', 'emailId', 'status'];
+  dataSource = ELEMENT_DATA;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+export interface PeriodicElement {
+  name: string;
+  username: string;
+  role: string;
+  emailId: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+  {name: 'Hydrogen', username: 'abc', role: 'Admin', emailId: 'abc@abc.com', status: 'Active'},
+];

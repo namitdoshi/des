1e9d0ef143856e72b8926f14b1cde53664697e12
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-09-22 20:08:47 +0530

    view allocation created and changes in examination list containar

--- src/app/paper-allocation/paper-allocation.component.ts
+++ src/app/paper-allocation/paper-allocation.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ExaminationListComponent } from './examination-list/examination-list.component';
+import { ViewAllocationComponent } from './view-allocation/view-allocation.component';
+import { AllocateExaminationComponent } from './allocate-examination/allocate-examination.component';
+
 @Component({
   selector: 'app-paper-allocation',
   templateUrl: './paper-allocation.component.html',
--- src/app/paper-allocation/view-allocation/view-allocation.component.ts
+++ src/app/paper-allocation/view-allocation/view-allocation.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-allocation.component.scss']
 })
 export class ViewAllocationComponent implements OnInit {
+  displayedColumns = ['name', 'paperUploadStatus', 'viewPaper', 'approve'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
@@ -13,3 +15,23 @@ export class ViewAllocationComponent implements OnInit {
   }
 
 }
+
+export interface PeriodicElement {
+  name: string;
+  paperUploadStatus: string;
+  viewPaper: string;
+  approve: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Hydrogen', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Helium', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Lithium', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Beryllium', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Boron', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Carbon', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Nitrogen', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Oxygen', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Fluorine', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+  {name: 'Neon', paperUploadStatus: 'uploaded', viewPaper: 'View/ Download', approve: 'Approved'},
+];

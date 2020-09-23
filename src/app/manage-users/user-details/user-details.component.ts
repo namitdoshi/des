670ea1b8e06fa21606851949a46d3e1879098b83
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-09-23 20:19:10 +0530

    added password and select field in user-details component

--- src/app/manage-users/user-details/user-details.component.ts
+++ src/app/manage-users/user-details/user-details.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  hide = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Food {
+  value: string;
+  viewValue: string;
 }

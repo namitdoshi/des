bce03ac471a782e499e9da420cfd5849e072f056
Namit Doshi <namitdoshi90@gmail.com>
2020-10-02 17:35:50 +0530

    auth module form errors fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import {AuthComponent} from './auth.component';
 import {AuthRoutingModule} from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthComponent

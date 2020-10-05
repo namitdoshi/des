ec5d31d821ee6ce0837dcd89f72e3d1fea80a9e6
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-10-05 20:11:23 +0530

    admin user list table created

--- src/app/admin/ad-manage-users/ad-user-list/ad-user-list.component.ts
+++ src/app/admin/ad-manage-users/ad-user-list/ad-user-list.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ad-user-list.component.scss']
 })
 export class AdUserListComponent implements OnInit {
-
+  displayedColumns = ['name', 'username', 'role', 'emailId', 'status', 'edit'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+export interface PeriodicElement {
+  name: string;
+  username: string;
+  role: string;
+  emailId: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'Mukesh', username: 'scanner', role: 'SCANNER', emailId: 'abc@abc.com', status: 'Y'},
+];

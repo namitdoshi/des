15d564f3ea2a391c2cd39f5a928e8d3276f93425
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-10-05 20:14:48 +0530

    admin SUBJECT details form created

--- src/app/admin/manage-subjects/subject-details/subject-details.component.ts
+++ src/app/admin/manage-subjects/subject-details/subject-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-subject-details',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subject-details.component.scss']
 })
 export class SubjectDetailsComponent implements OnInit {
+  hide = true;
+
+  subjectDetail: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.subjectDetail = new FormGroup({
+      subjectCode: new FormControl(null),
+      subjectName: new FormControl(null),
+      subjectClass: new FormControl(null),
+      examYear: new FormControl(null),
+      semester: new FormControl(null),
+      stream: new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+
   }
 
 }

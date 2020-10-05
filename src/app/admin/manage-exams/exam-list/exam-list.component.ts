0a8a89c705816847831452360d1641def70f1f58
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-10-05 20:12:54 +0530

    admin exAM list table created

--- src/app/admin/manage-exams/exam-list/exam-list.component.ts
+++ src/app/admin/manage-exams/exam-list/exam-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-exam-list',
@@ -6,10 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exam-list.component.scss']
 })
 export class ExamListComponent implements OnInit {
+  displayedColumns = ['ExamCode', 'SubjectCode', 'ExamName', 'ExamDate', 'ResultDate', 'NumberofCopies', 'ScannedCopies',
+    'EvaluatedCopies', 'edit'];
+  dataSource = ELEMENT_DATA;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
+export interface PeriodicElement {
+  ExamCode: string;
+  SubjectCode: string;
+  ExamName: string;
+  ExamDate: string;
+  ResultDate: string;
+  NumberofCopies: string;
+  ScannedCopies: string;
+  EvaluatedCopies: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    ExamCode: 'MAC-CFY6-Q5HK-GG0V',
+    SubjectCode: 'MECH001',
+    ExamName: 'Machine Design - May 2020',
+    ExamDate: '2020-02-22',
+    ResultDate: '2020-01-30',
+    NumberofCopies: '100',
+    ScannedCopies: '2',
+    EvaluatedCopies: '1',
+  }, {
+    ExamCode: 'MAC-CFY6-Q5HK-GG0V',
+    SubjectCode: 'MECH001',
+    ExamName: 'Machine Design - May 2020',
+    ExamDate: '2020-02-22',
+    ResultDate: '2020-01-30',
+    NumberofCopies: '100',
+    ScannedCopies: '2',
+    EvaluatedCopies: '1',
+  }, {
+    ExamCode: 'MAC-CFY6-Q5HK-GG0V',
+    SubjectCode: 'MECH001',
+    ExamName: 'Machine Design - May 2020',
+    ExamDate: '2020-02-22',
+    ResultDate: '2020-01-30',
+    NumberofCopies: '100',
+    ScannedCopies: '2',
+    EvaluatedCopies: '1',
+  }, {
+    ExamCode: 'MAC-CFY6-Q5HK-GG0V',
+    SubjectCode: 'MECH001',
+    ExamName: 'Machine Design - May 2020',
+    ExamDate: '2020-02-22',
+    ResultDate: '2020-01-30',
+    NumberofCopies: '100',
+    ScannedCopies: '2',
+    EvaluatedCopies: '1',
+  }, {
+    ExamCode: 'MAC-CFY6-Q5HK-GG0V',
+    SubjectCode: 'MECH001',
+    ExamName: 'Machine Design - May 2020',
+    ExamDate: '2020-02-22',
+    ResultDate: '2020-01-30',
+    NumberofCopies: '100',
+    ScannedCopies: '2',
+    EvaluatedCopies: '1',
+  },
+];
+

cbf34de36ebe288b202c6950cb449429c2af12be
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-10-11 19:32:38 +0530

    Modal dialog box created

--- src/app/admin/manage-exams/exam-details/add-question-modal/add-question-modal.component.ts
+++ src/app/admin/manage-exams/exam-details/add-question-modal/add-question-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-add-question-modal',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-question-modal.component.scss']
 })
 export class AddQuestionModalComponent implements OnInit {
+  foods: Food[] = [
+    {value: 'Scanner', viewValue: 'Scanner'},
+    {value: 'Evaluator', viewValue: 'Evaluator'},
+  ];
+  // userDetail: FormGroup;
+  // userDetail2: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    // this.userDetail = new FormGroup({
+    //   name: new FormControl(null),
+    //   username: new FormControl(null),
+    //   email: new FormControl(null),
+    //   role: new FormControl(null),
+    //   password: new FormControl(null),
+    //   password2: new FormControl(null),
+    //   active: new FormControl({value: null, disabled: true})
+    // });
+    //
+    // this.userDetail2 = new FormGroup({
+    //   officeCode: new FormControl(null),
+    //   officeName: new FormControl(null),
+    //   contactPerson: new FormControl(null),
+    //   address: new FormControl(null),
+    //   phone: new FormControl(null),
+    //   mobileNumber: new FormControl(null)
+    // });
+  }
+
+}
+interface Food {
+  value: string;
+  viewValue: string;
 }
